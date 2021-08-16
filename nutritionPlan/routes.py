33e4cff5ebc0f85c51082a8a65878e785534532c
EugeneL97 <eugenejr14@gmail.com>
2021-08-15 18:39:49 -0700

    Login System Functionality Added

--- nutritionPlan/routes.py
+++ nutritionPlan/routes.py
@@ -1,7 +1,8 @@
 from nutritionPlan import app
 from nutritionPlan.models import AnswersDB, userInfo, db
-from nutritionPlan.forms import createAccForm
-from flask import render_template, request, redirect
+from nutritionPlan.forms import createAccForm, loginForm
+from flask import render_template, request, redirect, url_for, flash
+from flask_login import login_user, logout_user, login_required
 
 @app.route('/')
 @app.route('/home')
@@ -11,25 +12,47 @@ def homePage():
 @app.route('/createAccount', methods=['POST', 'GET'])
 def createAccount():
     form = createAccForm()
+    if form.validate_on_submit():
+        userToCreate = userInfo(username=form.username.data,
+                            emailAddress=form.emailAddress.data,
+                            passwordHashAlgo=form.password1.data)
+        db.session.add(userToCreate)
+        db.session.commit()
+
+        flash('User created successfully!', category='success')
+        return redirect(url_for('homePage'))
+    if form.error != {}:
+        for errorMsg in form.errors.values():
+            flash(f'Error with creating an account: {errorMsg}', category='danger')
     return render_template('createAccount.html', form=form)
 
 #Login -> form
 #Need to login in order to access the form
 @app.route('/login', methods=['POST', 'GET'])
 def checkLogin():
-    if request.method == "POST":
-        login = userInfo(username=request.form['username'], password=request.form['password'])
-        try:
-            #Check username and password in DB (if it exists)
-            #Need to understand syntax
-            db.query.filter_by(username)
-        except:
-            return "User does not exist."
-    else:
-        return render_template("login.html")
+    form = loginForm()
+    if form.validate_on_submit():
+        attemptedUser = userInfo.query.filter_by(username=form.username.data).first()
+        if attemptedUser and attemptedUser.check_password_correction(
+            attemptedPassword=form.password.data
+        ):
+            login_user(attemptedUser)
+            flash('Login successful!', category='success')
+            return redirect(url_for('homePage'))
+            
+        else:
+            flash('The username and password combo does not exist!', category='danger')
+    return render_template("login.html", form=form)
+
+@app.route('/logout')
+def logoutUser():
+    logout_user()
+    flash('Logged out successfully!', category='info')
+    return redirect(url_for("homePage"))
 
 #Form -> answers
 @app.route('/form', methods=['POST', 'GET'])
+@login_required
 def formPage():
     if request.method == "POST":
 
