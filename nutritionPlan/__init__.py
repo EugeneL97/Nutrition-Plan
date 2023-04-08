bf06ee43caa06a793d9f94f6d2d7629f4f4c763c
Eugene Lacatis <eugenejr14@gmail.com>
2023-04-08 11:45:35 -0700

    Going from SQL to NoSQL database

--- nutritionPlan/__init__.py
+++ nutritionPlan/__init__.py
@@ -1,13 +1,13 @@
 from flask import Flask#, render_template, url_for, request, redirect
-from flask_sqlalchemy import SQLAlchemy
+from pymongo import MongoClient
 #from wtforms import Form, TextField, BooleanField, validators
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///nutritionPlan.db'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017:nutritionPlan'
 app.config['SECRET_KEY'] = '2d700b21121a0917e03ae0ed'
-db = SQLAlchemy(app)
+mongo = MongoClient(app.config['MONGO_URI'])
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'checkLogin'
