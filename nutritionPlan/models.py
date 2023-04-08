bcb0af914f5fa5af78cb0b0fd2dc8d30f45dbb13
Eugene Lacatis <eugenejr14@gmail.com>
2023-04-08 15:59:00 -0700

    Redefine methods to align with MongoDB

--- nutritionPlan/models.py
+++ nutritionPlan/models.py
@@ -1,19 +1,22 @@
-from enum import unique
-from os import EX_TEMPFAIL
-from nutritionPlan import db, bcrypt, login_manager
+from nutritionPlan import mongo, bcrypt, login_manager
 from flask_login import UserMixin
+from pymongo import MongoClient
 
 @login_manager.user_loader
 def load_user(user_id):
-    return userInfo.query.get(int(user_id))
+    user_data = mongo.users.find_one({'id': int(user_id)})
+    if user_data:
+        return User(user_data)
+    return None
 
-class userInfo(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    emailAddress = db.Column(db.String(254), unique=True, nullable=False)
-    username = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    passwordSalt = db.Column(db.String(50), nullable=True)
-    passwordHash = db.Column(db.String(50))
+class user:
+    def __init__(self, user_data):
+        
+        self.id = str(user_data['id'])
+        self.emailAddress = user_data['emailAddress']
+        self.username = user_data['username']
+        self.passwordHash = user_data['passwordHash']
+        self.passwordSalt = user_data['passwordSalt']
 
     @property
     def password(self):
@@ -21,17 +24,17 @@ class userInfo(db.Model, UserMixin):
 
     @password.setter
     def password(self, plainTextPassword):
-        self.passwordHash = bcrypt.generate_password_hash(plainTextPassword).decode('utf-8')
+        self.passwordSalt = bcrypt.gensalt().decode('utf-8')
+        self.passwordHash = bcrypt.hashpw(plainTextPassword.encode('utf-8'), self.passwordSalt.encode('utf-8')).decode('utf-8')
 
     def check_password_correction(self, attemptedPassword):
-        return bcrypt.check_password_hash(self.passwordHash, attemptedPassword)
+        return bcrypt.checkpw(attemptedPassword.encode('utf-8'), self.passwordHash.encode('utf-8'))
 
-class Answers(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    sex = db.Column(db.String(length=1), default='M', nullable=False)
-    age = db.Column(db.SmallInteger, default=18, nullable=False)
-    heightInCm = db.Column(db.SmallInteger, default=172, nullable=False)
-    weightInKg = db.Column(db.SmallInteger, default=62, nullable=False)
-    activity = db.Column(db.SmallInteger, default=0, nullable=False)
-    meals = db.Column(db.SmallInteger, default=1,nullable=False)
-    snacks = db.Column(db.SmallInteger, default=0, nullable=False)
+class Answers(mongo.Document):
+    sex = mongo.StringField(default='M', required=True)
+    age = mongo.IntField(default=18, required=True)
+    heightInCm = mongo.IntField(default=172, required=True)
+    weightInKg = mongo.IntField(default=62, required=True)
+    activity = mongo.IntField(default=0, required=True)
+    meals = mongo.IntField(default=1, required=True)
+    snacks =mongo.IntField(default=0, required=True)
